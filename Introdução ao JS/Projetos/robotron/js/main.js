1c9ab4adffe6525220e13a7eef9a66f9e4c78172
Guilherme Barbosa <111584900+guilherme-barbosa-rodrigues@users.noreply.github.com>
2023-01-02 12:17:11 -0300

    up

---
+++ Introdução ao JS/Projetos/robotron/js/main.js
@@ -0,0 +1,9 @@
+const robotron = document.querySelector('#robotron');
+robotron.addEventListener("click", function() {
+    console.log('CLICOU')
+})
+function dizOi() {
+    console.log('oi')
+    console.log('Bem-vindo ao Robotron 2000')
+}
+dizOi()
