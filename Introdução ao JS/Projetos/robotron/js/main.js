feaebfb18dcd635f89c37f7944ab074ff9c777c9
Guilherme Barbosa <111584900+guilherme-barbosa-rodrigues@users.noreply.github.com>
2023-01-03 15:47:01 -0300

    uyp

--- Introdução ao JS/Projetos/robotron/js/main.js
+++ Introdução ao JS/Projetos/robotron/js/main.js
@@ -1,9 +1,18 @@
-const robotron = document.querySelector('#robotron');
-robotron.addEventListener("click", function() {
-    console.log('CLICOU')
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
+
+const controle = document.querySelectorAll(".controle-ajuste")
+controle.forEach( (elemento) => {
+    console.log(elemento)
 })
-function dizOi() {
-    console.log('oi')
-    console.log('Bem-vindo ao Robotron 2000')
+somar.addEventListener("click" , () => {manipulaDados("somar")})
+subtrair.addEventListener("click" , () => {manipulaDados("subtrair")})
+
+function manipulaDados (operacao) {
+    if(operacao === "somar") {
+        braco.value = parseInt(braco.value) + 1
+    } else {
+        braco.value = parseInt(braco.value) - 1 
+    }
 }
-dizOi()
